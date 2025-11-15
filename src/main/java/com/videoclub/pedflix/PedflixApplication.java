fdfd89d6f9b9342322df1f09877149a447e4c1e2
Pedro Sánchez <periko17188@hotmail.com>
2025-11-15 11:07:16 +0100

    Subo estructura inicial del proyecto Pedflix

---
+++ src/main/java/com/videoclub/pedflix/PedflixApplication.java
@@ -0,0 +1,13 @@
+package com.videoclub.pedflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videoclub/pedflix/PedflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoclub.pedflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
